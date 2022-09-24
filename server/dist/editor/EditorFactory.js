4f95e1c97c82aec6eeb9767163a8ff051df1272c
vanya vorobev <ivanvorobev14042004@mail.ru>
2022-09-25 00:21:37 +0700

    hello

--- server/dist/editor/EditorFactory.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=EditorFactory.js.map
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,37 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import videoRouter from "../routes/router"
 
-dotenv.config({path: "../.env"})
+import log4js from 'log4js'
+
+// controllers
+import Editor from './editor/Editor'
+import ChangeFExtentionContr from './controllers/ChangeFExtentionContr'
+import ChangeSizeContr from './controllers/ChangeSizeContr'
+
+const editor = new Editor;
+const changeFExtentionContr = new ChangeFExtentionContr(editor);
+const changeSizeContr = new ChangeSizeContr(editor);
+
+// dotenv
+dotenv.config()
+const logger = log4js.getLogger()
+logger.level = process.env.LOG_LEVEL || ''
+
 const app = express()
 const port = process.env.EXPRESS_PORT
 
+// app
 app.use(express.json())
 
-app.use(videoRouter)
+app.get('/', (request, response) => {
+    changeFExtentionContr.execute("mp3");
+})
+
+
+
+app.post('/api/video', (request, response) => {
+    console.log(request.body)
+    return response.sendStatus(200)
+})
 
 app.listen(port, () => console.log(`Running on port ${port}`))
