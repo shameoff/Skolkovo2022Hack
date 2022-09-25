003f8e0db360abe5947a287979ac8084cadba39a
vanya vorobev <ivanvorobev14042004@mail.ru>
2022-09-25 13:26:57 +0700

    fix cutFileComp

--- server/src/editor/components/CutFileComp.ts
+++ server/src/editor/components/CutFileComp.ts
@@ -20,13 +20,14 @@ export default class CutFileComp {
             this.ffmpeg.FS('writeFile', oldFileName, await this.fetchFile('./src/input_files/' + oldFileName));
             let concat: string = "concat:";
             for(let i = 0; i < timelines.length; i++) {
-                await this.ffmpeg.run('-ss', String(timelines[i].start),'-i', oldFileName,"-t", String(timelines[i].end), String(i)+".mp4");
+                await this.ffmpeg.run('-ss', String(timelines[i].start),'-i', oldFileName,"-t", String(timelines[i].end - timelines[i].start), String(i)+".mp4");
                 await this.ffmpeg.run('-i', String(i) + ".mp4", '-acodec', 'copy', '-vcodec', 'copy', '-vbsf', 'h264_mp4toannexb', '-f', 'mpegts', String(i) + ".ts");
                 concat += String(i) + ".ts";
                 if(i<=timelines.length) {
                     concat += "|";
                 }
             }
+            console.log("==================\n==================" + concat)
             await this.ffmpeg.run('-i', concat, '-vcodec', 'copy', '-acodec', 'copy', "buff.mp4");
             await this.ffmpeg.run('-i', "buff.mp4", newFileName);
             await this.fs.promises.writeFile('./src/output_files/' + newFileName, this.ffmpeg.FS('readFile', "buff.mp4"));
